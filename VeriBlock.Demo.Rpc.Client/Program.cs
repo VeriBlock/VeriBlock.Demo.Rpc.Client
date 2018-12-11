9bd761e206d1bdbcbe491f9a13c14b9ab29c0d38
timstall <timstall@hotmail.com>
2018-12-10 21:27:30 -0600

    Add project files.

---
+++ VeriBlock.Demo.Rpc.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VeriBlock.Demo.Rpc.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
